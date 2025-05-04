7a907e1ca13a8f2cffda79f2771eb2c7868c51f7
shirocodes <ishirapt@gmail.com>
2025-05-04 06:24:24 +0300

    Enabled bootstrap styling on layout/heading.

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <>
-      <header>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/book">Book Now</Link>
+      <header className="bg-light border-bottom">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+          <Link className="navbar-brand p-2" to="/">
+            LOGO
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto p-2">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/book">
+                  Book Now
+                </Link>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
 
-      <main>{children}</main>
+      <main className="container my-5">{children}</main>
 
-      <footer>
-        <p>© 2025 Behavior Analyst</p>
+      <footer className="bg-light text-center py-4 mt-auto">
+        <p className="mb-0">© 2025 Behavior Analyst</p>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter future={{
-    v7_relativeSplatPath: true,
-    v7_startTransition: true,
-  }}>
+    <BrowserRouter
+      future={{
+        v7_relativeSplatPath: true,
+        v7_startTransition: true,
+      }}
+    >
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
