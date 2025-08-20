0219f12398ef441900d30716ec3dd3e23d39942a
shirocodes <ishirapt@gmail.com>
2025-08-20 16:14:20 +0300

    deploy: Changed base API's url to link to backend's render url.

--- client/src/apis/Axios.jsx
+++ client/src/apis/Axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Base API URL 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { "Content-Type": "application/json" }
 });
 
