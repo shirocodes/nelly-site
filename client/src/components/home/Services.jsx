ec42413cdf0c7bed67c82ceadb104cf64c770c17
shirocodes <ishirapt@gmail.com>
2025-08-20 09:35:09 +0300

    refactor(servicespage): Added AoS animation and glowing background color

--- client/src/components/home/Services.jsx
+++ client/src/components/home/Services.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import ServiceCard from '../UiUx.jsx/ServicesCard'
 import classImg from '../../assets/class-img.jpg'
 import homeImg from '../../assets/home-basedImg.jpg'
@@ -33,12 +35,24 @@ const therapyServices = [
 ]
 
 const Services = () => {
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true }) // fade-in duration
+  }, [])
   return (
-    <section className='container my-5'>
+    <section className='container my-5' 
+      style={{background: "linear-gradient(145deg,rgb(249, 247, 249), rgb(222, 238, 223))",
+          boxShadow: "0 0 20px rgba(0, 150, 136, 0.4), 0 0 40px rgba(0, 150, 136, 0.2)",
+   }}
+    >
       <h2 className='text-center mb-4'>Our Behavior Therapy Services</h2>
       <div className='row g-4'>
         {therapyServices.map((service, index) => (
-          <div key={index} className='col-md-4'>
+          <div 
+            key={index} 
+            className='col-12 col-md-6 col-lg-6'
+            data-aos="fade-up"       //\ Add animation
+            data-aos-delay={index * 100} // stagger effect
+          >
             <ServiceCard {...service}/>
           </div>
         ))}
