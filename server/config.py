cb8791d9583cc3664bce78c0128f5166241d3af8
shirocodes <ishirapt@gmail.com>
2025-08-20 14:05:56 +0300

    deploy: Added requirements.txt for prod dep.

--- server/config.py
+++ server/config.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+db_url = os.getenv("DATABASE_URL")
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
