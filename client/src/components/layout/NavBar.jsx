17f68adf06bfa58d760224abca2c53e49154c15e
shirocodes <ishirapt@gmail.com>
2025-08-20 09:16:01 +0300

    feat(navbar): Enabled togglebutton to show and disappear with actions.

--- client/src/components/layout/NavBar.jsx
+++ client/src/components/layout/NavBar.jsx
@@ -1,16 +1,23 @@
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import abaImg from "../../assets/aba-img.png";
-import { AuthContext } from "../../context/AuthContext"
+import { AuthContext } from "../../context/AuthContext";
 
 const NavBar = () => {
-
-const { user, logout } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     await logout();
     navigate("/");
+  };
+
+  // Close navbar after clicking a link (for mobile)
+  const closeNavbar = () => {
+    const navbar = document.getElementById("navbarNav");
+    if (navbar && navbar.classList.contains("show")) {
+      navbar.classList.remove("show");
+    }
   };
 
   return (
@@ -20,7 +27,7 @@ const { user, logout } = useContext(AuthContext);
         style={{ position: "sticky", top: 0, zIndex: 1030 }}
       >
         <nav className="navbar navbar-expand-lg py-0 child-nav">
-          <Link className="navbar-brand p-2" to="/">
+          <Link className="navbar-brand p-2" to="/" onClick={closeNavbar}>
             <img
               src={abaImg}
               alt="Little Puzzles ABA Therapy"
@@ -41,51 +48,58 @@ const { user, logout } = useContext(AuthContext);
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto p-2">
               <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link className="nav-link" to="/" onClick={closeNavbar}>
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/services">
+                <Link className="nav-link" to="/services" onClick={closeNavbar}>
                   Services
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link ms-2 mt-1 btn-book" to="/book">
+                <Link className="nav-link" to="/book" onClick={closeNavbar}>
                   Book Now
                 </Link>
               </li>
               {!user ? (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/signup">
-                    Signup
-                  </Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/account">
-                    My Account
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <button
-                    className="nav-link btn btn-link"
-                    style={{ textDecoration: "none" }}
-                    onClick={handleLogout}
-                  >
-                    Logout
-                  </button>
-                </li>
-              </>
-            )}
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login" onClick={closeNavbar}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/signup" onClick={closeNavbar}>
+                      Signup
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to="/account"
+                      onClick={closeNavbar}
+                    >
+                      My Account
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <button
+                      className="nav-link btn btn-link"
+                      style={{ textDecoration: "none" }}
+                      onClick={() => {
+                        handleLogout();
+                        closeNavbar();
+                      }}
+                    >
+                      Logout
+                    </button>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </nav>
