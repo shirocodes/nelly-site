314ef09e33094a733c527dc58470669856032469
shirocodes <ishirapt@gmail.com>
2025-08-21 11:54:40 +0300

    debug: updated Flask-CORS configs for Render deploy.

--- server/app.py
+++ server/app.py
@@ -11,6 +11,7 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(Config)
     
     CORS(app,
      resources={r"/*": {"origins": [
